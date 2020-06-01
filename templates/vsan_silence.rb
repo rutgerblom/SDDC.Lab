87d9727ea8f833ab1729fdf1d77bf5550d506687
Rutger Blom <rutgerblom@Rutgers-iMac.local>
2020-06-01 16:51:10 +0200

    added silencing

--- templates/vsan_silence.rb
+++ templates/vsan_silence.rb
@@ -13,6 +13,8 @@ rvc_exec("vsan.health.silent_health_check_configure -a controllerdriver #{vcente
 rvc_exec("vsan.health.silent_health_check_configure -a controllerfirmware #{vcenter_name}/#{datacenter_name}/computers/#{cluster_name}")
 rvc_exec("vsan.health.silent_health_check_configure -a controllerreleasesupport #{vcenter_name}/#{datacenter_name}/computers/#{cluster_name}")
 rvc_exec("vsan.health.silent_health_check_configure -a controlleronhcl #{vcenter_name}/#{datacenter_name}/computers/#{cluster_name}")
+rvc_exec("vsan.health.silent_health_check_configure -a autohclupdate #{vcenter_name}/#{datacenter_name}/computers/#{cluster_name}")
+rvc_exec("vsan.health.silent_health_check_configure -a releasecataloguptodate #{vcenter_name}/#{datacenter_name}/computers/#{cluster_name}")
 rvc_exec("vsan.health.silent_health_check_configure -a perfsvcstatus #{vcenter_name}/#{datacenter_name}/computers/#{cluster_name}")
 rvc_exec("vsan.health.silent_health_check_configure -a smalldiskstest #{vcenter_name}/#{datacenter_name}/computers/#{cluster_name}")
 rvc_exec("vsan.health.silent_health_check_configure -a vsanenablesupportinsight #{vcenter_name}/#{datacenter_name}/computers/#{cluster_name}")
