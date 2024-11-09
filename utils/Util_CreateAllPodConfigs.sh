b441a54619e930103f5b50d3433c4c5867417ae8
Luis Chanu <luischanu@users.noreply.github.com>
2024-11-09 15:56:34 -0800

    Added IP range for user deployed appliances in README.md

--- utils/Util_CreateAllPodConfigs.sh
+++ utils/Util_CreateAllPodConfigs.sh
@@ -23,9 +23,10 @@
 
 # Loop through config files
 for file in config*.yml; do
-    # Process all config.* files except config_sample.yml
+    # Process all config*.yml files except config_sample.yml
     if [[ -f "$file" ]] && [[ $file != "config_sample.yml" ]]; then
         echo -e "Processing Config File: \033[33m$file\033[0m"
         ansible-playbook -e "SourceConfigPath=$(pwd) SourceConfigFile=$file" playbooks/CreatePodConfig.yml 1> /dev/null &
+        sleep 2
     fi
 done
